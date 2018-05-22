aeee3430278ff5cdf705ab9c1b07820b634c7f84
Chaim Lev-Ari <chiptus@gmail.com>
2018-05-22 23:11:24 +0700

    move to client

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
   render() {
     return (
