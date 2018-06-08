022b8572a08d3ba50e1d99a24751d2a5e2b4f8b3
Chaim Lev-Ari <chiptus@gmail.com>
2018-06-08 14:04:45 +0200

    configure navigation

--- client/App.js
+++ client/App.js
@@ -1,17 +1,27 @@
 // import React from 'react';
 
-import { createStackNavigator } from 'react-navigation';
-import Home from './components/home-screen/home-screen';
+import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import Add from './components/add-combination/new-suit-screen';
 import Suit from './components/suit/suit';
 import Gallery from './components/gallery-screen/gallery-screen';
 import Closet from './components/closet-screen/closet-screen';
 
-export default createStackNavigator(
-  { Home, Add, Suit, Gallery, Closet },
+export default createSwitchNavigator(
+  {
+    Add,
+    Closet: createStackNavigator(
+      {
+        Suit,
+        Gallery,
+        Closet,
+      },
+      { initialRouteName: 'Closet' },
+    ),
+  },
   { initialRouteName: 'Closet' },
 );
 
+/* global __DEV__ */
 if (__DEV__) {
   // eslint-disable-next-line global-require
   require('react-native').YellowBox.ignoreWarnings([
